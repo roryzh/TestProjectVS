a4a05b5fcb86269dd64a23901fc118975ebc8571
Rory <zrr666@gmail.com>
2018-03-27 18:57:21 -0500

    Initial check-in of module TestProject

---
+++ TestProject/Controllers/PropertyController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TestProject.Models;
+
+// For more information on enabling MVC for empty projects, visit 
+
+namespace TestProject.Controllers
+{
+    public class PropertyController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Display()
+        {
+            var property = new Property() {Address = "XXX", Price = 1000 };
+            ViewData["Message"] = "Your contact page.";
+            return View(property);
+        }
+
+        public void getData(){
+
+
+        }
+    }
+}
---
+++ TestProject/Models/Property.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace TestProject.Models
+{
+    public class Property
+    {
+        public string Address { get; set; }
+        public int Price { get; set; }
+    }
+}
