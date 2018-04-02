6b6fc4e1ba0282d9777bcb1a59e9343e3b478e2b
Rory <zrr666@gmail.com>
2018-04-02 08:58:52 -0500

    Second commit

--- TestProject/Controllers/PropertyController.cs
+++ TestProject/Controllers/PropertyController.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Collections;
 using Microsoft.AspNetCore.Mvc;
 using TestProject.Models;
+
 
 // For more information on enabling MVC for empty projects, visit 
 
@@ -11,17 +13,30 @@ namespace TestProject.Controllers
 {
     public class PropertyController : Controller
     {
-        // GET: /<controller>/
+        // GET: /<controller>
+
+
         public IActionResult Display()
         {
-            var property = new Property() {Address = "XXX", Price = 1000 };
+
+            var property1 = new Property() { Address = "2102 Kevin St, Tx", Year = 1990, Sqft=1100, Bedroom=1, Bathroom=1, Price = 100000 };
+            var property2 = new Property() { Address = "3102 Belter St, Tx", Year = 1991, Sqft=1200, Bedroom = 2, Bathroom = 2, Price = 200000 };
+            var property3 = new Property() { Address = "4102 Good St, Tx", Year = 1992, Sqft=1300, Bedroom = 3, Bathroom = 3, Price = 300000 };
+
+            //ArrayList myProperties = new ArrayList();
+            var myProperties = new List<Property>();
+            myProperties.Add(property1);
+            myProperties.Add(property2);
+            myProperties.Add(property3);
+
+
             ViewData["Message"] = "Your contact page.";
-            return View(property);
+            return View(myProperties);
         }
 
-        public void getData(){
+        //public void getData(){
 
 
-        }
+        //}
     }
 }
--- TestProject/Models/Property.cs
+++ TestProject/Models/Property.cs
@@ -4,6 +4,11 @@ namespace TestProject.Models
     public class Property
     {
         public string Address { get; set; }
+        public int Year { get; set; }
+        public int Sqft { get; set; }
+        public int Bedroom { get; set; }
+        public int Bathroom { get; set; }
         public int Price { get; set; }
+
     }
 }
