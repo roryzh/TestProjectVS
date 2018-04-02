150198efa5c89fbd5356e11055f5c7b05134ff15
Rory <rzhuang@CougarNet.UH.EDU>
2018-04-02 16:48:05 -0500

    third button

--- TestProject/Controllers/PropertyController.cs
+++ TestProject/Controllers/PropertyController.cs
@@ -19,9 +19,9 @@ namespace TestProject.Controllers
         public IActionResult Display()
         {
 
-            var property1 = new Property() { Address = "2102 Kevin St, Tx", Year = 1990, Sqft=1100, Bedroom=1, Bathroom=1, Price = 100000 };
-            var property2 = new Property() { Address = "3102 Belter St, Tx", Year = 1991, Sqft=1200, Bedroom = 2, Bathroom = 2, Price = 200000 };
-            var property3 = new Property() { Address = "4102 Good St, Tx", Year = 1992, Sqft=1300, Bedroom = 3, Bathroom = 3, Price = 300000 };
+            var property1 = new Property() { Address = "2102 Kevin St, Houston, Tx", Year = 1990, Sqft=1100, Bedroom=1, Bathroom=1, Price = 100000 };
+            var property2 = new Property() { Address = "3102 Belter St, Houston, Tx", Year = 1991, Sqft=1200, Bedroom = 2, Bathroom = 2, Price = 200000 };
+            var property3 = new Property() { Address = "4102 Good St, Houston, Tx", Year = 1992, Sqft=1300, Bedroom = 3, Bathroom = 3, Price = 300000 };
 
             //ArrayList myProperties = new ArrayList();
             var myProperties = new List<Property>();
@@ -34,9 +34,10 @@ namespace TestProject.Controllers
             return View(myProperties);
         }
 
-        //public void getData(){
+        public int getData(){
 
+            return 123;
 
-        //}
+        }
     }
 }
