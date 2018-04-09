986bce39c154ee9e1ae4b12ab7ed26fde60c7d7b
Ruoyu Zhuang <rzhuang@CougarNet.UH.EDU>
2018-04-09 15:47:53 -0500

    add search and textbox callback

--- TestProject/Controllers/PropertyController.cs
+++ TestProject/Controllers/PropertyController.cs
@@ -18,26 +18,29 @@ namespace TestProject.Controllers
 
         public IActionResult Display()
         {
+            var myProperties = new List<Property>();
 
-            return '123';
+            return View(myProperties);
         }
 
-        public IActionResult getData(){
+        [HttpPost]
+        public IActionResult getData(String zipcode){
 
-            //return 123;
-            var property1 = new Property() { Address = "2102 Kevin St, Houston, Tx", Year = 1990, Sqft = 1100, Bedroom = 1, Bathroom = 1, Price = 100000 };
-            var property2 = new Property() { Address = "3102 Belter St, Houston, Tx", Year = 1991, Sqft = 1200, Bedroom = 2, Bathroom = 2, Price = 200000 };
-            var property3 = new Property() { Address = "4102 Good St, Houston, Tx", Year = 1992, Sqft = 1300, Bedroom = 3, Bathroom = 3, Price = 300000 };
-
-            //ArrayList myProperties = new ArrayList();
             var myProperties = new List<Property>();
-            myProperties.Add(property1);
-            myProperties.Add(property2);
-            myProperties.Add(property3);
 
+            if (Convert.ToInt32(zipcode) == 5)
+            {
+                var property1 = new Property() { Address = "2102 Kevin St, Houston, Tx", Year = 1990, Sqft = 1100, Bedroom = 1, Bathroom = 1, Price = 100000 };
+                var property2 = new Property() { Address = "3102 Belter St, Houston, Tx", Year = 1991, Sqft = 1200, Bedroom = 2, Bathroom = 2, Price = 200000 };
+                var property3 = new Property() { Address = "4102 Good St, Houston, Tx", Year = 1992, Sqft = 1300, Bedroom = 3, Bathroom = 3, Price = 300000 };
 
-            ViewData["Message"] = "Your contact page.";
-            return View(myProperties);
+                //ArrayList myProperties = new ArrayList();
+                myProperties.Add(property1);
+                myProperties.Add(property2);
+                myProperties.Add(property3);
+
+            }
+            return View("Display", myProperties);
         }
     }
 }
